3e588e01c0389008e6884ca4277c57cf94ab31ce
reentrant <sd.engineer@gmail.com>
2016-08-31 16:27:56 -0500

    Enhanced comments in helper functions

--- uart.c
+++ uart.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 void UART_Init(void) {
-
     TXSTAbits.TX9 = 0; // select 8 data bits
     TXSTAbits.TXEN = 1; // enable transmit
-    TXSTAbits.SYNC = 0;
+    TXSTAbits.SYNC = 0; // -> configures the EUSART1 for asynchronous operation.
 
     RCSTAbits.SPEN = 1; // serial port is enabled
     RCSTAbits.RX9 = 0; // select 8 data bits
 
     RCSTAbits.CREN = 1; // receive enabled
 
-    SPBRG = 16; // here is calculated value of SPBRGH and SPBRGL 115200 @ 32MHz FOSC
-    TXSTAbits.BRGH = 1;
-    BRG16 = 0;
+    //SPBRG = 16; // here is calculated value of SPBRGH and SPBRGL 115200 @ 32MHz FOSC
+    SPBRG = 207; // here is calculated value of SPBRGH and SPBRGL 9600 @ 32MHz FOSC
+    TXSTAbits.BRGH = 1; // -> 
+    BRG16 = 0;          // -> 
 
     PIR1bits.RCIF = 0; // make sure receive interrupt flag is clear
     PIE1bits.RCIE = 0; // enable UART Receive interrupt
